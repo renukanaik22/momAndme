b380f39eaebbb9d6a3de73151847b85de88cfbb5
renukanaik22 <renukanaik2203@gmail.com>
2026-02-05 00:15:53 +0530

    feat: added backend basic structure

---
+++ backend/src/main/java/com/momandme/MomAndMeApplication.java
@@ -0,0 +1,12 @@
+package com.momandme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomAndMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomAndMeApplication.class, args);
+    }
+}
